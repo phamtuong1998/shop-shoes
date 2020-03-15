6d5f67349dc8fd8ee73feb5cc47b673e0e0676d5
phamtuong <phamtuong123@gmail.com>
2020-03-15 17:43:57 +0700

    fix: header

--- common/header.php
+++ common/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE HTML>
 <html>
 	<head>
-	<title>phamtuong</title>
+	<title>Phạm Tưởng</title>
    <meta charset="utf-8">
    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
