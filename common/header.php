b5caf13e93690493cd94a147bce7f7275e3080c0
phamtuong <phamtuong123@gmail.com>
2020-03-15 17:41:04 +0700

    feat: init code

---
+++ common/header.php
@@ -0,0 +1,106 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+	<title>phamtuong</title>
+   <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600,700" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Rokkitt:100,300,400,700" rel="stylesheet">
+	
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="../css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="../css/icomoon.css">
+	<!-- Ion Icon Fonts-->
+	<link rel="stylesheet" href="../css/ionicons.min.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="../css/bootstrap.min.css">
+
+	<!-- Magnific Popup -->
+	<link rel="stylesheet" href="../css/magnific-popup.css">
+
+	<!-- Flexslider  -->
+	<link rel="stylesheet" href="../css/flexslider.css">
+
+	<!-- Owl Carousel -->
+	<link rel="stylesheet" href="../css/owl.carousel.min.css">
+	<link rel="stylesheet" href="../css/owl.theme.default.min.css">
+	
+	<!-- Date Picker -->
+	<link rel="stylesheet" href="../css/bootstrap-datepicker.css">
+	<!-- Flaticons  -->
+	<link rel="stylesheet" href="../fonts/flaticon/font/flaticon.css">
+
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="../css/style.css">
+
+	</head>
+	<body>
+		
+	<div class="colorlib-loader"></div>
+
+	<div id="page">
+		<nav class="colorlib-nav" role="navigation">
+			<div class="top-menu">
+				<div class="container">
+					<div class="row">
+						<div class="col-sm-7 col-md-9">
+							<div id="colorlib-logo"><a href="/">ShosePT</a></div>
+						</div>
+						<div class="col-sm-5 col-md-3">
+			            <form action="#" class="search-wrap">
+			               <div class="form-group">
+			                  <input type="search" class="form-control search" placeholder="Tìm kiếm">
+			                  <button class="btn btn-primary submit-search text-center" type="submit"><i class="icon-search"></i></button>
+			               </div>
+			            </form>
+			         </div>
+		         </div>
+					<div class="row">
+						<div class="col-sm-12 text-left menu-1">
+							<ul>
+								<li class="active"><a href="/">Trang Chủ</a></li>
+								<li class="has-dropdown">
+									<a href="men.php">Sản Phẩm</a>
+									<ul class="dropdown">
+										<li><a href="product-detail.php">adidas</a></li>
+										<li><a href="cart.php">Nike</a></li>
+										<li><a href="checkout.php">Gucci</a></li>
+										<li><a href="order-complete.php">Balenciaga</a></li>
+										<li><a href="add-to-wishlist.php">Puma</a></li>
+									</ul>
+								</li>
+								<li><a href="women.php">Giày Nam</a></li>
+								<li><a href="women.php">Giày Nữ</a></li>
+								<li><a href="about.php">Hàng Likenew</a></li>
+								<li><a href="contact.php">Liên Hệ</a></li>
+								<li class="cart"><a href="cart.php"><i class="icon-shopping-cart"></i> Giỏ Hàng [0]</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="sale">
+				<div class="container">
+					<div class="row">
+						<div class="col-sm-8 offset-sm-2 text-center">
+							<div class="row">
+								<div class="owl-carousel2">
+									<div class="item">
+										<div class="col">
+											<h3><a href="#">Sản phẩm đang được giảm giá 25%.</a></h3>
+										</div>
+									</div>
+									<div class="item">
+										<div class="col">
+											<h3><a href="#">Sản phẩm HOT nhất hiện nay.</a></h3>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</nav>
